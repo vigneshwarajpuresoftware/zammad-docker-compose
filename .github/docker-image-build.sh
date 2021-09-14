80d7c0c69adcd0428f3535c18540f552199f1e7f
A J Vigneshwar <86348043+vigneshwarajpuresoftware@users.noreply.github.com>
2021-09-14 14:52:17 +0530

    Update docker-image-build.sh

--- .github/docker-image-build.sh
+++ .github/docker-image-build.sh
@@ -10,5 +10,6 @@ DOCKER_IMAGES="zammad zammad-elasticsearch zammad-postgresql"
 # shellcheck disable=SC2153
 for DOCKER_IMAGE in ${DOCKER_IMAGES}; do
   echo "Build Zammad Docker image ${DOCKER_IMAGE} for local or ci tests"
+  docker --version
   docker build --pull --no-cache --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t "${DOCKER_IMAGE}-local" -f "containers/${DOCKER_IMAGE}/Dockerfile" .
 done
