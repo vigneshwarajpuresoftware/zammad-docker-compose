cf6631a5d696190cd9d8e732b15d4a0a1b65a8e3
A J Vigneshwar <86348043+vigneshwarajpuresoftware@users.noreply.github.com>
2021-09-14 15:58:26 +0530

    Update docker-image-build.sh

--- .github/docker-image-build.sh
+++ .github/docker-image-build.sh
@@ -12,7 +12,7 @@ for DOCKER_IMAGE in ${DOCKER_IMAGES}; do
   echo "Build Zammad Docker image ${DOCKER_IMAGE} for local or ci tests"
   docker --version
   docker buildx create --name samplekit
-  docker buildx use smaplekit
+  docker buildx use samplekit
   docker buildx inspect --bootstrap
   docker buildx build --pull --push --no-cache --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t ajv21/zammad-docker-compose:latest --platform linux/arm64,linux/amd64 -f "containers/${DOCKER_IMAGE}/Dockerfile" .
   docker buildx rm samplekit
