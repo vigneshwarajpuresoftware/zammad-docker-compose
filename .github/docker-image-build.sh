dabb96de9e4536f58bc8c03a2da91e9e01ab35eb
A J Vigneshwar <86348043+vigneshwarajpuresoftware@users.noreply.github.com>
2021-09-14 16:04:01 +0530

    Update docker-image-build.sh

--- .github/docker-image-build.sh
+++ .github/docker-image-build.sh
@@ -14,6 +14,8 @@ for DOCKER_IMAGE in ${DOCKER_IMAGES}; do
   docker buildx create --name samplekit
   docker buildx use samplekit
   docker buildx inspect --bootstrap
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  docker login --username=ajv21 -p=Jun21@2021
   docker buildx build --pull --push --no-cache --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t ajv21/zammad-docker-compose:latest --platform linux/arm64,linux/amd64 -f "containers/${DOCKER_IMAGE}/Dockerfile" .
   docker buildx rm samplekit
 done
